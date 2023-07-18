453f39214ce20f236b3c8b089b57cc55011b5a86
Damir Rakipov <anas.rakipov@mail.ru>
2023-07-18 16:25:35 +0500

    build: createapp

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,130 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div style="display: flex">
+        <div class="left-panel">
+            <div v-for="list in lists" :key="list.id" class="list">
+                <div class="list-header" @click="toggleListCollapse(list)">
+                    <span>{{ list.name }}</span>
+                    <button>{{ list.collapsed ? 'Развернуть' : 'Свернуть' }}</button>
+                </div>
+                <div v-if="!list.collapsed">
+                    <input type="checkbox" v-model="list.selectAll" @change="toggleSelectAll(list)">
+                    <span>Выбрать все</span>
+                    <div v-for="item in list.items" :key="item.id" class="item">
+                        <input type="checkbox" v-model="item.selected">
+                        <input type="number" v-model.number="item.quantity" min="0">
+                        <input type="color" v-model="item.color">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right-panel">
+            <div v-for="list in sortedLists" :key="list.id" class="list">
+                <h3>{{ list.name }}</h3>
+                <div v-for="item in list.items" :key="item.id" class="item">
+                    <div v-for="n in item.quantity" :key="n" class="square" :style="{ backgroundColor: item.color }"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: 'ItemList',
+    data() {
+        return {
+            lists: [
+                {
+                    id: 1,
+                    name: 'List 1',
+                    selectAll: false,
+                    collapsed: false,
+                    items: [
+                        { id: 1, quantity: 2, color: '#ff0000', selected: false },
+                        { id: 2, quantity: 3, color: '#00ff00', selected: false },
+                        { id: 3, quantity: 1, color: '#0000ff', selected: false },
+                    ]
+                },
+                {
+                    id: 2,
+                    name: 'List 2',
+                    selectAll: false,
+                    collapsed: false,
+                    items: [
+                        { id: 1, quantity: 2, color: '#ff0000', selected: false },
+                        { id: 2, quantity: 3, color: '#00ff00', selected: false },
+                        { id: 3, quantity: 1, color: '#0000ff', selected: false },
+                    ]
+                },
+                {
+                    id: 3,
+                    name: 'List 3',
+                    selectAll: false,
+                    collapsed: false,
+                    items: [
+                        { id: 1, quantity: 2, color: '#ff0000', selected: false },
+                        { id: 2, quantity: 3, color: '#00ff00', selected: false },
+                        { id: 3, quantity: 1, color: '#0000ff', selected: false },
+                    ]
+                },
+            ]
+        };
+    },
+    methods: {
+        toggleSelectAll(list) {
+            const allSelected = list.items.every(item => item.selected);
+            list.items.forEach(item => {
+                item.selected = !allSelected;
+            });
+        },
+        sortList(list) {
+            list.items.sort((a, b) => a.id - b.id);
+        },
+        shuffleList(list) {
+            for (let i = list.items.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [list.items[i], list.items[j]] = [list.items[j], list.items[i]];
+            }
+        },
+        toggleListCollapse(list) {
+            list.collapsed = !list.collapsed;
+        },
+    },
+    computed: {
+        sortedLists() {
+            return this.lists.map(list => {
+                const sortedList = { ...list };
+                if (sortedList.sorted) {
+                    this.sortList(sortedList);
+                } else {
+                    this.shuffleList(sortedList);
+                }
+                return sortedList;
+            });
+        },
+    },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.list {
+    margin-bottom: 10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.square {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
 }
-a {
-  color: #42b983;
+.left-panel{
+    width: 50%;
+}
+.right-panel{
+    width: 50%;
 }
 </style>
